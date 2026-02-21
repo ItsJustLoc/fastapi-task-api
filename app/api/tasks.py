03e3b58cdd52c6eb2ff976e09300e4ae10820ce0
Loc Nguyen <theboiloc@gmail.com>
2026-02-21 00:49:41 -0600

    Add read task endpoints

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
 
 from app.api.deps import get_db
 from app.models.task import Task
@@ -13,3 +14,16 @@ async def newTask(task_in: TaskCreate, db = Depends(get_db)):
     db.commit()
     db.refresh(new_task)
     return new_task
+
+@router.get("/tasks", response_model=list[TaskRead])
+async def getAllTasks(db = Depends(get_db)):
+    tasks = db.scalars(select(Task).order_by(Task.id.desc())).all()
+    return tasks
+
+@router.get("/tasks/{task_id}", response_model=TaskRead)
+async def getTask(task_id: int, db = Depends(get_db)):
+    task = db.scalars(select(Task).where(Task.id == task_id)).first()
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
